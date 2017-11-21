eb5cb15e839d28d77224f5f4dd842e691cc7d561
douglasbeav <dobe@easj.dk>
2017-11-21 12:05:54 +0100

    A new class - updated test class
    A new class - DB_Statements - for video - Database and MySQL 4 - 
    Also an updated test.java

--- test.java
+++ test.java
@@ -1,7 +1,12 @@
-
 public class test {
 
     public static void main(String[] args) {
-        DB_Connector.connect();
+//        DB_Connector.connect();
+        DB_Statements stmts = new DB_Statements();
+//        stmts.createDB();
+        stmts.useDB();
+//        stmts.createTable("ThisDatabase", "ThisTable");
+//        stmts.insertData();
+//        stmts.queryThisTable();
     }
 }
