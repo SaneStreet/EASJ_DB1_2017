1f1cd98f35634ed24755b68eb07afac2e7b07432
douglasbeav <dobe@easj.dk>
2018-03-06 22:37:23 +0100

    1st commit for PostgreSQL_DB project

---
+++ src/Connection_Test.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+
+public class Connection_Test {
+
+    public static void main(String[] args) {
+
+        //  Test DB_Connector
+//        Connection conn = DB_Connector.connect();
+
+        //  Create a DB_Connector object
+//        DB_Connector connect = new DB_Connector();
+
+        //  Test DB_Statements - insertData and retrieveData methods
+        DB_Statements statements = new DB_Statements();
+        statements.insertData();
+        statements.retrieveData();
+
+
+
+    }
+}
